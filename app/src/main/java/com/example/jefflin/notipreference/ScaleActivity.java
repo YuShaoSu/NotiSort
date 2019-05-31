6c0316f62b23171c6431dd7bdafbc08a9cad3e69
林子傑 <jefflin@linzijiede-MacBook-Pro.local>
2019-05-31 21:48:14 +0800

    add and try surveylib

--- app/src/main/java/com/example/jefflin/notipreference/ScaleActivity.java
+++ app/src/main/java/com/example/jefflin/notipreference/ScaleActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidadvance.androidsurvey.SurveyActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ScaleActivity extends AppCompatActivity {
 
     private ImageView iv_back;
@@ -28,6 +33,7 @@ public class ScaleActivity extends AppCompatActivity {
             }
         });
 
+        /*
         tv_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +41,30 @@ public class ScaleActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        */
+
+        final int SURVEY_REQUEST = 1337;
+        tv_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i_survey = new Intent(ScaleActivity.this, SurveyActivity.class);
+                i_survey.putExtra("json_survey", loadSurveyJson("example_survey_1.json"));
+                startActivityForResult(i_survey, SURVEY_REQUEST);
+            }
+        });
+    }
+
+    private String loadSurveyJson(String filename) {
+        try {
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
